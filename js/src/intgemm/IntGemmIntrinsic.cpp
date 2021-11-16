ebc6f6f0d009b29aba2ce2983b4a44bbe606f72c
Abhishek Aggarwal <aaggarwal@mozilla.com>
2021-11-16 15:31:19 +0100

    Cleaned up js/src/intgemm/IntGemmIntrinsic.cpp file
     - Removed commented out code

--- js/src/intgemm/IntGemmIntrinsic.cpp
+++ js/src/intgemm/IntGemmIntrinsic.cpp
@@ -20,10 +20,6 @@ int32_t js::intgemm::intrSample1(Instance* instance, uint32_t arr, uint32_t len,
                                  uint8_t* memBase) {
   MOZ_ASSERT(SASigIntrSample1.failureMode == FailureMode::FailOnNegI32);
 
-  // const int8_t* A = (int8_t*)10;
-  // int8_t* output_addr = (int8_t*)calloc(10, sizeof(int8_t));;
-  //::intgemm::Int8::PrepareBQuantizedTransposed(A, output_addr, 5, 6);
-
 #if INTGEMM_INTR_SHARED
   const SharedArrayRawBuffer* rawBuf =
       SharedArrayRawBuffer::fromDataPtr(memBase);
@@ -50,53 +46,6 @@ int32_t js::intgemm::intrSample1(Instance* instance, uint32_t arr, uint32_t len,
 
   return 0;
 }
-
-/*
-
-void Int8ShiftedMultiplyExport(const int8_t* A, const int8_t* B, int A_rows,
-                               int width, int B_cols, float unquant_mult,
-                               const float* bias_addr, float* output_addr) {
-  intgemm::Int8Shift::Multiply(A, B, A_rows, width, B_cols,
-                               intgemm::callbacks::UnquantizeAndAddBiasAndWrite(
-                                   unquant_mult, bias_addr, output_addr));
-}
-
-void Int8PrepareAExport(const float* A, int8_t* output_addr, float quant_mult,
-                        int rows, int cols) {
-  intgemm::Int8::PrepareA(A, output_addr, quant_mult, rows, cols);
-}
-
-void Int8ShiftedPrepareAExport(const float* A, int8_t* output_addr,
-                               float quant_mult, int rows, int cols) {
-  intgemm::Int8Shift::PrepareA(A, output_addr, quant_mult, rows, cols);
-}
-
-void Int8PrepareBExport(const float* A, int8_t* output_addr, float quant_mult,
-                        int rows, int cols) {
-  intgemm::Int8::PrepareB(A, output_addr, quant_mult, rows, cols);
-}
-
-void Int8ShiftedPrepareBiasExport(const int8_t* A, int rows, int cols,
-                                  float unquant_mult, const float* bias_addr,
-                                  float* output_addr) {
-  intgemm::Int8Shift::PrepareBias(
-      A, rows, cols,
-      intgemm::callbacks::UnquantizeAndAddBiasAndWrite(unquant_mult, bias_addr,
-                                                       output_addr));
-}
-
-void Int8MultiplyExport(const int8_t* A, const int8_t* B, int A_rows, int width,
-                        int B_cols, float unquant_mult, float* output_addr) {
-  intgemm::Int8::Multiply(
-      A, B, A_rows, width, B_cols,
-      intgemm::callbacks::UnquantizeAndWrite(unquant_mult, output_addr));
-}
-
-void Int8PrepareBQuantizedTransposedExport(const int8_t* A, int8_t* output_addr,
-                                           int rows, int cols) {
-  intgemm::Int8::PrepareBQuantizedTransposed(A, output_addr, rows, cols);
-}
-*/
 
 int32_t js::intgemm::intrI8PrepareB(wasm::Instance* instance,
                                     uint32_t inputMatrixB, float scale,
